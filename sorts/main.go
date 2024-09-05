cb4e166c1ba1dedb2a15999bcf720e53f20b0a39
Khanh Nguyen <pkhanhit@gmail.com>
2024-09-05 01:21:35 -0400

    feat: made use of sort

---
+++ sorts/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	xi := []int{4, 7, 3, 42, 99, 18, 16, 56, 12}
+	xs := []string{"James", "Q", "M", "Moneypenny", "Dr. No"}
+
+	fmt.Println("Before sort", xi)
+	sort.Ints(xi)
+	fmt.Println("After sort", xi)
+	fmt.Println("Before sort", xs)
+	sort.Strings(xs)
+	fmt.Println("After sort", xs)
+
+	people := []struct {
+		Name string
+		Age  int
+	}{
+		{"Gopher", 7},
+		{"Alice", 55},
+		{"Vera", 24},
+		{"Bob", 75},
+	}
+	sort.Slice(people, func(i, j int) bool { return people[i].Name < people[j].Name })
+	fmt.Println("By name:", people)
+
+	sort.Slice(people, func(i, j int) bool { return people[i].Age < people[j].Age })
+	fmt.Println("By age:", people)
+}
